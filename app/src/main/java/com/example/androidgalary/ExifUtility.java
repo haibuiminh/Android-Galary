e8ef7475d7ec6e1408d2ad0944124634fd9539ae
haibuiminh <haibui@kms-technology.com>
2021-12-09 18:33:16 +0700

    add fetching location

--- app/src/main/java/com/example/androidgalary/ExifUtility.java
+++ app/src/main/java/com/example/androidgalary/ExifUtility.java
@@ -4,6 +4,8 @@ import static com.example.androidgalary.MainActivity.viewPager;
 
 import androidx.exifinterface.media.ExifInterface;
 
+import org.json.JSONObject;
+
 import java.io.File;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -18,56 +20,97 @@ public class ExifUtility {
 
         File f = new File(AnhFragment.mangHinh.get(viewPager.getCurrentItem()).duongdan);
 
-        if (!getExifTag(exif, ExifInterface.TAG_DATETIME).isEmpty()){
+        if (!getExifTag(exif, ExifInterface.TAG_DATETIME).isEmpty()) {
             exifList.add("Date: " + getExifTag(exif, ExifInterface.TAG_DATETIME));
-        }
-        else {
+        } else {
             DateFormat dateTimeInstance = SimpleDateFormat.getDateTimeInstance();
             exifList.add("Date: " + dateTimeInstance.format(new Date(f.lastModified())));
         }
-        if (!getExifTag(exif, ExifInterface.TAG_FILE_SOURCE).isEmpty()){
+        if (!getExifTag(exif, ExifInterface.TAG_FILE_SOURCE).isEmpty()) {
             exifList.add("File Source: " + getExifTag(exif, ExifInterface.TAG_FILE_SOURCE));
         }
         exifList.add("File Size: " + f.length() / 1024 + "KB");
 
-        if (!getExifTag(exif, ExifInterface.TAG_IMAGE_WIDTH).isEmpty() && !getExifTag(exif, ExifInterface.TAG_IMAGE_LENGTH).isEmpty()){
+        if (!getExifTag(exif, ExifInterface.TAG_IMAGE_WIDTH).isEmpty() && !getExifTag(exif, ExifInterface.TAG_IMAGE_LENGTH).isEmpty()) {
             exifList.add("Image Size: " + getExifTag(exif, ExifInterface.TAG_IMAGE_WIDTH) + "x" + getExifTag(exif, ExifInterface.TAG_IMAGE_LENGTH));
         }
-        if (!getExifTag(exif, ExifInterface.TAG_MAKE).isEmpty()){
+        if (!getExifTag(exif, ExifInterface.TAG_MAKE).isEmpty()) {
             exifList.add("Make: " + getExifTag(exif, ExifInterface.TAG_MAKE));
         }
-        if (!getExifTag(exif, ExifInterface.TAG_MODEL).isEmpty()){
+        if (!getExifTag(exif, ExifInterface.TAG_MODEL).isEmpty()) {
             exifList.add("Model: " + getExifTag(exif, ExifInterface.TAG_MODEL));
         }
-        if (!getExifTag(exif, ExifInterface.TAG_WHITE_BALANCE).isEmpty()){
+        if (!getExifTag(exif, ExifInterface.TAG_WHITE_BALANCE).isEmpty()) {
             exifList.add("White Balance: " + getExifTag(exif, ExifInterface.TAG_WHITE_BALANCE));
         }
-        if (!getExifTag(exif, ExifInterface.TAG_BRIGHTNESS_VALUE).isEmpty()){
+        if (!getExifTag(exif, ExifInterface.TAG_BRIGHTNESS_VALUE).isEmpty()) {
             exifList.add("Brightness: " + getExifTag(exif, ExifInterface.TAG_BRIGHTNESS_VALUE));
         }
-        if (!getExifTag(exif, ExifInterface.TAG_CONTRAST).isEmpty()){
+        if (!getExifTag(exif, ExifInterface.TAG_CONTRAST).isEmpty()) {
             exifList.add("Contrast: " + getExifTag(exif, ExifInterface.TAG_CONTRAST));
         }
-        if (!getExifTag(exif, ExifInterface.TAG_GAMMA).isEmpty()){
+        if (!getExifTag(exif, ExifInterface.TAG_GAMMA).isEmpty()) {
             exifList.add("Gamma: " + getExifTag(exif, ExifInterface.TAG_GAMMA));
         }
-        if (!getExifTag(exif, ExifInterface.TAG_SATURATION).isEmpty()){
+        if (!getExifTag(exif, ExifInterface.TAG_SATURATION).isEmpty()) {
             exifList.add("Saturation: " + getExifTag(exif, ExifInterface.TAG_SATURATION));
         }
-        if (!getExifTag(exif, ExifInterface.TAG_SHARPNESS).isEmpty()){
+        if (!getExifTag(exif, ExifInterface.TAG_SHARPNESS).isEmpty()) {
             exifList.add("Sharpness: " + getExifTag(exif, ExifInterface.TAG_SHARPNESS));
         }
-        if (!getExifTag(exif, ExifInterface.TAG_FOCAL_LENGTH).isEmpty()){
+        if (!getExifTag(exif, ExifInterface.TAG_FOCAL_LENGTH).isEmpty()) {
             exifList.add("Focal Length: " + getExifTag(exif, ExifInterface.TAG_FOCAL_LENGTH));
         }
-        if (!getExifTag(exif, ExifInterface.TAG_ISO_SPEED).isEmpty()){
+        if (!getExifTag(exif, ExifInterface.TAG_ISO_SPEED).isEmpty()) {
             exifList.add("ISO Speed: " + getExifTag(exif, ExifInterface.TAG_ISO_SPEED));
         }
-        if (!getExifTag(exif, ExifInterface.TAG_SHUTTER_SPEED_VALUE).isEmpty()){
+        if (!getExifTag(exif, ExifInterface.TAG_SHUTTER_SPEED_VALUE).isEmpty()) {
             exifList.add("Shutter Speed: " + getExifTag(exif, ExifInterface.TAG_SHUTTER_SPEED_VALUE));
         }
 
+        String location = GetLocationExif(exif);
+        if (!location.isEmpty()){
+            exifList.add("Location: " + location);
+        }
+
         return exifList;
+    }
+
+    private String GetLocationExif(ExifInterface exif) {
+
+        try {
+            String latitudeRef, longitudeRef;
+            Float latitude = 0.f;
+            Float longitude = 0.f;
+
+            if (exif.getAttribute(ExifInterface.TAG_GPS_LATITUDE_REF) != null) {
+                latitudeRef = exif.getAttribute(ExifInterface.TAG_GPS_LATITUDE_REF);
+                if (latitudeRef.equals("N")) {
+                    latitude = convertFromDegreeMinuteSeconds(exif.getAttribute(ExifInterface.TAG_GPS_LATITUDE));
+                } else {
+                    latitude = 0 - convertFromDegreeMinuteSeconds(exif.getAttribute(ExifInterface.TAG_GPS_LATITUDE));
+                }
+            }
+
+            if (exif.getAttribute(ExifInterface.TAG_GPS_LONGITUDE_REF) != null) {
+                longitudeRef = exif.getAttribute(ExifInterface.TAG_GPS_LONGITUDE_REF);
+                if (longitudeRef.equals("E")) {
+                    longitude = convertFromDegreeMinuteSeconds(exif.getAttribute(ExifInterface.TAG_GPS_LONGITUDE));
+                } else {
+                    longitude = 0 - convertFromDegreeMinuteSeconds(exif.getAttribute(ExifInterface.TAG_GPS_LONGITUDE));
+                }
+            }
+
+            String location = new LocationFetch(latitude, longitude).execute().get();
+
+            JSONObject jsonObj = new JSONObject(location);
+            return jsonObj.getString("display_name");
+
+        } catch (Exception e) {
+            return "";
+        }
+
+
     }
 
     private static Float convertFromDegreeMinuteSeconds(String stringDMS) {
